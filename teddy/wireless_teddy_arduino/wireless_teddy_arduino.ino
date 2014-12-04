61143ab192a7b3f7f110a19aab45d92a0849f071
Andrew Sczesnak <andrewscz@gmail.com>
2014-12-04 15:56:35 -0800

    Latest version of Arduino side of wireless teddy

--- teddy/wireless_teddy_arduino/wireless_teddy_arduino.ino
+++ teddy/wireless_teddy_arduino/wireless_teddy_arduino.ino
@@ -5,6 +5,7 @@
 #include "Wire.h"
 #include "I2Cdev.h"
 #include "MPU6050.h"
+#include "math.h"
 
 // radio
 RF24 radio(9, 10);
@@ -13,6 +14,7 @@ const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
 // gyro
 MPU6050 accelgyro(0x68);
 int16_t vals[6];
+float gyro[2];
 
 // LEDs
 #define DATA_PIN (5)
@@ -24,6 +26,12 @@ uint32_t eyecolor;
 
 #define pinModeFast(x, y) pinMode(x, y)
 #define digitalWriteFast(x, y) digitalWrite(x, y)
+
+int32_t lr_color[6] = {255,0,0,0,255,0};
+int32_t c_color[3] = {0,0,0};
+
+bool idle = 0;
+int8_t idle_inc = 2;
 
 static void set_pixel(uint8_t index, uint32_t color) {
   pixels[index] = color & 0x00FFFFFF;
@@ -69,6 +77,28 @@ static void show() {
   delay(1);
 }
 
+int twos_complement(int val)
+{
+  return (val ^ 0xFFFFFFFF) + 1;
+}
+
+double dist(float a, float b)
+{
+  return sqrt((a * a) + (b * b));
+}
+
+void get_vals(int16_t data[6], float *rot_x, float *rot_y)
+{
+  float ax, ay, az;
+ 
+  ax = twos_complement(data[0]) / 16384.0;
+  ay = twos_complement(data[1]) / 16384.0;
+  az = twos_complement(data[2]) / 16384.0;
+  
+  *rot_y = -degrees(atan2(ax, dist(ay, az)));
+  *rot_x = -degrees(atan2(ay, dist(ax, az)));
+}
+
 void setup()
 {
   Serial.begin(9600);
@@ -83,12 +113,12 @@ void setup()
   radio.begin();
   radio.setRetries(15, 15);
   radio.setPayloadSize(16);
-  radio.printDetails();
-  
   radio.openWritingPipe(pipes[0]);
   radio.openReadingPipe(1, pipes[1]);
   
   radio.startListening();
+  
+  radio.printDetails();
 
   // LEDs
   pinModeFast(DATA_PIN, OUTPUT);
@@ -101,27 +131,60 @@ void loop()
 {
   // gyro
   accelgyro.getMotion6(&vals[0], &vals[1], &vals[2], &vals[3], &vals[4], &vals[5]);
+  get_vals(vals, &gyro[0], &gyro[1]);
   
   // send
   radio.stopListening();
-  radio.write(&vals, 6 * sizeof(int16_t));
+  radio.write(&gyro, sizeof(gyro));
   radio.startListening();
   
-  delay(10);
-      
+  delay(1);
+  
+  // figure out eye color
+  if (idle)
+  {
+    c_color[0] += idle_inc;
+    c_color[1] = 0;
+    c_color[2] = 0;
+    
+    if (c_color[0] <= 0)
+    {
+      idle_inc = abs(idle_inc);
+      c_color[0] = 0;
+    } else if (c_color[0] >= 255) {
+      idle_inc = -1 * abs(idle_inc);
+      c_color[0] = 255; 
+    }
+  } else {
+    float tilt = ((gyro[0] + 60) > 0 ? (gyro[0] + 60) : 1.0) / 120.0;
+    
+    if (tilt < 0.1)
+    {
+      tilt = 0.0;
+    } else if (tilt > 1.0) {
+      tilt = 1.0;
+    }
+  
+    c_color[0] = (uint8_t)((tilt * (float)lr_color[0]) + ((1.0-tilt) * (float)lr_color[3]));
+    c_color[1] = (uint8_t)((tilt * (float)lr_color[1]) + ((1.0-tilt) * (float)lr_color[4]));
+    c_color[2] = (uint8_t)((tilt * (float)lr_color[2]) + ((1.0-tilt) * (float)lr_color[5]));
+  }
+  
+  for (int i = 0; i < LIGHT_COUNT; i++)
+  {
+    set_pixel_rgb(i, c_color[2], c_color[1], c_color[0]);
+  }
+  
+  show();
+
   // receive
   if (radio.available())
   {
-    radio.read(&eyecolor, sizeof(uint32_t));
+    radio.read(&lr_color, 6 * sizeof(lr_color[0]));
     
-    // LEDs
-    for (int i = 0; i < LIGHT_COUNT; i++)
+    if (lr_color == 0)
     {
-      set_pixel(i, eyecolor);
+      idle = ~idle; 
     }
-    
-    show();
   }
-  
-  delay(10);
 }
